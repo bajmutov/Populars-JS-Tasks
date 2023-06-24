31f9dfb7ab082bf4042c19ae93dd4291c15b44b5
Oleksandr <bajmutov@gmail.com>
2023-06-24 17:35:13 +0300

    v2 - 1 dz

--- js/task-01.js
+++ js/task-01.js
@@ -1,1 +1,12 @@
-js;
+const categoriesElement = document.querySelector("#categories");
+
+console.log(`Number of categories: ${categoriesElement.children.length}`);
+console.log("");
+
+const numberCategories = [...categoriesElement.children].forEach(
+  (item, _, array) => {
+    console.log(`Category: ${item.firstElementChild.textContent}`);
+    console.log(`Elements: ${item.lastElementChild.children.length}`);
+    console.log("");
+  }
+);
