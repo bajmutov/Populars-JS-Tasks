a946fe81ebc63f2e4414a31e2146191ed5974d64
Oleksandr <bajmutov@gmail.com>
2023-06-28 08:52:25 +0300

    Update task-02.js
    task2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,14 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listElements = ingredients.map(fruit => {
+  const listItem = document.createElement("li");
+  listItem.textContent = `${fruit}`;
+  listItem.classList.add('item');
+  return listItem;
+})
+
+// console.log(listElements);
+const list = document.querySelector("#ingredients");
+list.append(...listElements);
