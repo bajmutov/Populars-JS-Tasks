1155b8cd88cf10b9eab8956904bebe8acd829e2e
Oleksandr <bajmutov@gmail.com>
2023-06-29 12:51:50 +0300

    Update task-06.js
    task6b

--- js/task-06.js
+++ js/task-06.js
@@ -2,6 +2,10 @@ const textInput = document.querySelector("#validation-input");
 
 textInput.addEventListener("blur", () => {
   if(textInput.value.length == textInput.dataset.length){
-    textInput.classList.toggle('valid');
-  } else textInput.classList.toggle('invalid')
+    textInput.classList.add('valid');
+    textInput.classList.remove('invalid');
+  } else {
+    textInput.classList.add('invalid');
+     textInput.classList.remove('valid');
+  }
   });
