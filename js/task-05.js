11dc8070622ee405eece978aab5dbd7a08923f64
Oleksandr <bajmutov@gmail.com>
2023-06-29 10:27:20 +0300

    Update task-05.js
    task5

---
+++ js/task-05.js
@@ -0,0 +1,7 @@
+const textInput = document.querySelector('#name-input');
+const output = document.querySelector('#name-output');
+
+textInput.addEventListener("input", (event) => {
+  if (!event.currentTarget.value) output.textContent='Anonymous';
+  else output.textContent = event.currentTarget.value;
+});
