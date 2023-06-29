8e9cd9b2bfb585c5c727ef48708305a8372221bc
Oleksandr <bajmutov@gmail.com>
2023-06-29 16:57:18 +0300

    Update task-09.js
    task9

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,17 @@
+const widgetElement = document.querySelector(".widget");
+const changeColorButton = document.querySelector(".change-color");
+const nameColor = document.querySelector(".color");
+const bgc = document.body;
+
+changeColorButton.addEventListener("click", onChangeBgkClick);
+
+function onChangeBgkClick() {
+  bgc.style.backgroundColor = getRandomHexColor();
+  nameColor.textContent = bgc.style.backgroundColor;
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
-}
+   }
